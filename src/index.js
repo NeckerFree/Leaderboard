7fab668d8f965faf65ef8ed2f44ac35bf85f798c
Elio Cortes <elio.cortes3000@gmail.com>
2022-04-13 18:34:05 -0500

    Implemented getScoreData

--- src/index.js
+++ src/index.js
@@ -3,55 +3,56 @@ import StoreScore from './modules/storeScore.js';
 
 const form = document.getElementsByTagName('form')[0];
 const storeScore = new StoreScore();
-const getInitialData = () => {
-  const scoreData = storeScore.getScoreData();
-  const scoreList = document.createElement('div');
-  scoreList.classList.add('scoreList');
-  if (scoreData !== null) {
+const getInitialData =async () => {
+  const scoreData =await storeScore.getScoreData();
+  const scoreList = document.getElementsByClassName('scoreList')[0];
+  if (scoreData!==undefined && scoreData !== null) {
     for (let index = 0; index < scoreData.length; index += 1) {
       const isRowPair = index % 2;
       const element = scoreData[index];
       const p = document.createElement('p');
-      p.innerText = `${element.name}: ${element.value}`;
+      p.innerText = `${element.user}: ${element.score}`;
       if (isRowPair === 0) {
         p.classList.add('alternateRow');
       } else {
         p.classList.add('normalRow');
       }
-
       scoreList.appendChild(p);
     }
   }
-  return scoreList;
 };
-const processData = () => {
+const processData =async () => {
   const inputName = document.getElementById('name');
   const inputScore = document.getElementById('score');
-  storeScore.add(inputName.value, inputScore.value);
+  const msg=await storeScore.add(inputName.value, inputScore.value);
 };
-const refresh = () => {
-  window.location.reload();
+const refresh =async () => {
+ let response=await getInitialData();
 };
 window.addEventListener('load', () => {
   const section = document.getElementsByTagName('section')[0];
   section.classList.add('sectionContainer');
-  const columnRecent = document.createElement('div');
+  const columnRecent = document.getElementsByTagName('article')[0];
   columnRecent.classList.add('columnRecent');
-  const rowRecent = document.createElement('div');
+  const divs=document.getElementsByTagName('div');
+  const rowRecent = divs[0];
   rowRecent.classList.add('recentScores');
-  const h2 = document.createElement('h2');
-  h2.innerText = 'Recent scores';
-  const refreshButton = document.createElement('input');
-  refreshButton.setAttribute('type', 'button');
-  refreshButton.setAttribute('id', 'refreshButton');
-  refreshButton.setAttribute('value', 'Refresh');
+  // const h2 = document.createElement('h2');
+  // h2.innerText = 'Recent scores';
+  const refreshButton = document.getElementById('refreshButton');
+  // refreshButton.setAttribute('type', 'button');
+  // refreshButton.setAttribute('id', 'refreshButton');
+  // refreshButton.setAttribute('value', 'Refresh');
   refreshButton.addEventListener('click', refresh);
-  rowRecent.appendChild(h2);
-  rowRecent.appendChild(refreshButton);
-  const scoreList = getInitialData();
-  columnRecent.appendChild(rowRecent);
-  columnRecent.appendChild(scoreList);
-  section.insertAdjacentElement('afterbegin', columnRecent);
+  // rowRecent.appendChild(h2);
+  // rowRecent.appendChild(refreshButton);
+  
+  const divScoreList = divs[1];
+  divScoreList.classList.add('scoreList');
+  let response=refresh();
+  // columnRecent.appendChild(rowRecent);
+  // columnRecent.appendChild(scoreList);
+  //section.insertAdjacentElement('afterbegin', columnRecent);
 });
 
 form.addEventListener('submit', () => {
--- src/modules/storeScore.js
+++ src/modules/storeScore.js
@@ -1,19 +1,67 @@
 import Storage from './storage.js';
 import Score from './score.js';
 
+const apiURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    //mode: 'cors',
+    //credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+    // redirect: 'follow',
+    // referrerPolicy: 'no-referrer',
+   
+  })
+  let answer = await response.json();
+  return answer;
+}
+const getData =async (url) => {
+  const response= await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json' },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  })
+  let data = await response.json();
+  return data;
+};
 /**
- * Class to Add, Remove and Get score from Storage from
+ * Class to Add, Remove and Get score from Storage 
  */
 export default class StoreScore {
-    scoreData;
-
-    constructor() { this.scoreData = new Storage('ScoresData'); }
-
-  add= (name, value) => {
-    this.tasksCollection = this.scoreData.getItemStorage();
-    const objectScore = new Score(name, value);
-    this.scoreData.addItemStorage(objectScore);
+  gameId = 'c3jiAaUZsctiWm4Cl82';
+  // constructor() {
+  //   let answer;
+  //   return (async () => {
+  //     if (this.gameId === '') {
+       
+  //       answer = await postData(`${apiURL}/games`, { name: 'Awesome Game' });
+  //       if (answer !== undefined && answer !== null) {
+  //         this.gameId = answer.result.match(/([^(Game with ID: )])(.*)[^( added.)]/g);
+  //       }
+  //     }
+  //     return answer;
+  //   });
+  //     // .then(data => data.json())
+  //     // .then(data => {
+  //     //   if (data !== undefined && data !== null) {
+  //     //     this.gameId = data.result.match(/([^(Game with ID: )])(.*)[^( added.)]/g);
+  //     //   }
+  //     // })
+   
+  // }
+  add =async (name, value) => {
+    let answer;
+    answer= await postData(`${apiURL}/games/${this.gameId}/scores`, { 'user': `${name}`, 'score': value });
+    return answer.result;
   }
-
-  getScoreData=() => this.scoreData.getItemStorage();
+  getScoreData = async () => {
+    let answer=[];
+    answer= await getData(`${apiURL}/games/${this.gameId}/scores`);
+    return answer.result;
+  }
 }
